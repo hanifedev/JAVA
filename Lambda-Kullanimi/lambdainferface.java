87066bfda65ed4947ba87cb6d9b7ba38e823604d
hanifekurnaz <hanifekurnaz@outlook.com>
2016-09-25 01:34:55 +0300

    lambda kullanimi

---
+++ Lambda-Kullanimi/lambdainferface.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author hanife
+ */
+public interface Fonksiyon {
+    double sayi(double x,double y);
+}
